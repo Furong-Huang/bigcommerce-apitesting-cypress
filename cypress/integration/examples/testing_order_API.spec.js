6ad45558afe6347269e273015ea48bee0ed04cbf
Furong Huang <furonghuang1314@outlook.com>
2021-03-01 11:56:01 +1300

    add changing order status

--- cypress/integration/examples/testing_order_API.spec.js
+++ cypress/integration/examples/testing_order_API.spec.js
@@ -1,58 +1,76 @@
-describe("order API testing",()=>{
+describe("order API testing", () => {
+  it("order API testing", function () {
+    //Create a awaiting Fulfillment Order
+    cy.request({
+      method: "Post",
+      url: "https://api.bigcommerce.com/stores/mjuebx3f0s/v2/orders",
+      headers: {
+        Accept: "application/json",
+        "Content-Type": "application/json",
+        "X-Auth-Token": "1phjqx8808ipjdid4xqte2y47yav07u",
+      },
+      body: {
+        billing_address: {
+          city: "Austin",
+          country: "United States",
+          country_iso2: "US",
+          email: "janedoe@email.com",
+          first_name: "Jane",
+          last_name: "Doe",
+          state: "Texas",
+          street_1: "123 Main Street",
+          zip: "78751",
+        },
+        products: [
+          {
+            name: "BigCommerce Coffee Mug",
+            price_ex_tax: 45,
+            price_inc_tax: 50,
+            quantity: 1,
+          },
+        ],
+      },
+    })
+      .then((response) => {
+        expect(response.status).to.equal(201);
+      })
+      .as("response");
 
-    it("order API testing", function() {
-
-      //Create a awaiting Fulfillment Order
+    //Get an order with created order id
+    cy.then(() => {
       cy.request({
-        method: "Post",
-        url: "https://api.bigcommerce.com/stores/mjuebx3f0s/v2/orders",
+        method: "Get",
+        url:
+          "https://api.bigcommerce.com/stores/mjuebx3f0s/v2/orders/" +
+          this.response.body.id,
+        headers: {
+          Accept: "application/json",
+          "Content-Type": "application/json",
+          "X-Auth-Token": "1phjqx8808ipjdid4xqte2y47yav07u",
+        },
+      }).then((response) => {
+        expect(response.status).to.equal(200);
+      });
+    });
+
+    //Changing order status
+    cy.then(() => {
+      cy.request({
+        method: "Put",
+        url:
+          "https://api.bigcommerce.com/stores/mjuebx3f0s/v2/orders/" +
+          this.response.body.id,
         headers: {
           Accept: "application/json",
           "Content-Type": "application/json",
           "X-Auth-Token": "1phjqx8808ipjdid4xqte2y47yav07u",
         },
         body: {
-          billing_address: {
-            city: "Austin",
-            country: "United States",
-            country_iso2: "US",
-            email: "janedoe@email.com",
-            first_name: "Jane",
-            last_name: "Doe",
-            state: "Texas",
-            street_1: "123 Main Street",
-            zip: "78751",
-          },
-          products: [
-            {
-              name: "BigCommerce Coffee Mug",
-              price_ex_tax: 45,
-              price_inc_tax: 50,
-              quantity: 1,
-            },
-          ],
+          status_id: 2,
         },
-      }).then(response =>{
-        expect(response.status).to.equal(201)
-      }).as('response')
-
-
-      //Get an order with created order id
-      cy.then(()=>{
-        cy.request({
-          method: "Get",
-          url: "https://api.bigcommerce.com/stores/mjuebx3f0s/v2/orders/"+ this.response.body.id,
-          headers: {
-            Accept: "application/json",
-            "Content-Type": "application/json",
-            "X-Auth-Token": "1phjqx8808ipjdid4xqte2y47yav07u",
-          },
-        }).then((response)=>{
-          expect(response.status).to.equal(200)
-        })
-      })
-
+      }).then((response) => {
+        expect(response.status).to.equal(200);
+      });
     });
-
-
-})
+  });
+});
